4e23c9b79c50cbfbcfbe95a623938acbd3e87bde
Gabriel Coutinho <gcoutinhorj@gmail.com>
2023-10-16 14:12:02 -0300

    initial structure

---
+++ login-app-ios/Components/SColors.swift
@@ -0,0 +1,13 @@
+//
+//  SShapeStyle.swift
+//  login-app-ios
+//
+//  Created by Gabriel Castelo De Azeredo Coutinho on 16/10/23.
+//
+
+import SwiftUI
+
+extension Color {
+    static let stoneGreen = Color.green
+    static let stoneButtonBorder = Color.gray
+}
