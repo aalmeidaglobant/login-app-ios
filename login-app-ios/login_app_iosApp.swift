c52d5fa65a6c9ce5dc85570b7fa0d41e48bccfe0
Gabriel Coutinho <gcoutinhorj@gmail.com>
2023-10-20 16:11:58 -0300

    add scene phases for clarity

--- login-app-ios/login_app_iosApp.swift
+++ login-app-ios/login_app_iosApp.swift
@@ -9,9 +9,24 @@ import SwiftUI
 
 @main
 struct login_app_iosApp: App {
+    @Environment(\.scenePhase) private var scenePhase
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+        .onChange(of: scenePhase) { oldValue, newValue in
+            print("\(oldValue) -> \(newValue)")
+            switch newValue {
+            case .active:
+                return
+            case .background:
+                return
+            case .inactive:
+                return
+            @unknown default:
+                return
+            }
         }
     }
 }
